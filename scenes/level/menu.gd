34b8c97eab93b76a7c6c2a1e1f64af7630b5e190
Kyle Dellock <dellocky1@gmail.com>
2025-06-10 19:48:43 -0400

    modified:   resources/settings.json 	modified:   scenes/level/menu.gd

--- scenes/level/menu.gd
+++ scenes/level/menu.gd
@@ -3,22 +3,22 @@ var settings_node
 var assignevent
 func _ready() -> void:
 	settings_node = get_node("data/settings")
-	var value = settings_node.json_data["key"]
+	var key_name = settings_node.json_data["key"]  # e.g., "D"
 
-	if value.contains("MOUSE_BUTTON"):
-		pass
+	# Define the action you're rebinding
+	var action_name = "key"  # This should match the action in your Input Map (Editor > Project Settings > Input Map)
 
+	if key_name == "MOUSE_BUTTON":
+		pass  # You said ignore this for now
 	else:
+		# Create a key input event
 		assignevent = InputEventKey.new()
-		(assignevent as InputEventKey).set_keycode(
-			OS.find_keycode_from_string(
-				value
-						)
-					)
-		(assignevent as InputEventKey).set_pressed(true)
+		assignevent.keycode = OS.find_keycode_from_string(key_name)
+		assignevent.pressed = true
 
-	InputMap.action_erase_events(value)
-	InputMap.action_add_event(value, assignevent)
+		# Clear old events for the action, and add new key event
+		InputMap.action_erase_events(action_name)
+		InputMap.action_add_event(action_name, assignevent)
 	
 func _process(_delta_time):
 	if Input.is_action_pressed("key"):
